9c0ec93e915dc010ac4281170e557f12b594dcbe
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-08-09 13:01:05 +0300

    feat: add preliminary layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="app">
-   
+    <div class="app__container">
+      <Form />
+    </div>   
   </div>
 </template>
 
 <script>
+import Form from './components/Form';
 
 export default {
   name: 'App',
+  data() {
+    return {};
+  },
+  components: {
+    Form,
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .app {
+    &__container {
+      max-width: 1200px;
+      width: 100%;
+      height: 100vh;
+      margin: 0 auto;
+      padding: 2rem;
+      font-size: 1.6rem;
+      background-color: rgb(163, 228, 222);
+    };
+  };
 </style>

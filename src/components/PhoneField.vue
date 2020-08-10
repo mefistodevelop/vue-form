0a87044f79b252e8580d909217dd26df6bd25413
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-08-10 14:30:52 +0300

    refactor: replace v-if with v-show

--- src/components/PhoneField.vue
+++ src/components/PhoneField.vue
@@ -4,13 +4,13 @@
      <span class="phone-field__label-header">
         {{ label }}
         <span class="phone-field__star" v-if="required">*</span>
-        <small class="phone-field__error" v-if="v.$error && !v.$required">
+        <small class="phone-field__error" v-show="v.$error && !v.$required">
           Это поле обязательно для заполнения
         </small>
-        <small class="phone-field__error" v-if="v.length">
+        <small class="phone-field__error" v-show="v.length">
           Номер телефона должен быть 11 цифр
         </small>
-        <small class="phone-field__error" v-if="v.firstCharIs">
+        <small class="phone-field__error" v-show="v.firstCharIs">
           Номер телефона должен начинаться с цифры 7
         </small>
      </span>
@@ -34,7 +34,7 @@
     props: {
       value: {
         type: String,
-        default: ""
+        default: '',
       },
       v: {
         type: Object,
